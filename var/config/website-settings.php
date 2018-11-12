4331955913efee16f62207fdfe9a26e2ab025b02
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-11-12 11:45:16 +0100

    add config files

---
+++ var/config/website-settings.php
@@ -0,0 +1,54 @@
+<?php 
+
+return [
+    1 => [
+        "id" => 1,
+        "name" => "name",
+        "language" => "",
+        "type" => "text",
+        "data" => "Pimcore GmbH",
+        "siteId" => 0,
+        "creationDate" => 0,
+        "modificationDate" => 1524042159
+    ],
+    2 => [
+        "id" => 2,
+        "name" => "address",
+        "language" => "",
+        "type" => "text",
+        "data" => "Söllheimstrasse 16",
+        "siteId" => 0,
+        "creationDate" => 0,
+        "modificationDate" => 1524042178
+    ],
+    3 => [
+        "id" => 3,
+        "name" => "zipCity",
+        "language" => "",
+        "type" => "text",
+        "data" => "5020 Salzburg",
+        "siteId" => 0,
+        "creationDate" => 0,
+        "modificationDate" => 1524042194
+    ],
+    4 => [
+        "id" => 4,
+        "name" => "phone",
+        "language" => "",
+        "type" => "text",
+        "data" => "+43 662 876606230",
+        "siteId" => 0,
+        "creationDate" => 0,
+        "modificationDate" => 1524042209
+    ],
+    5 => [
+        "id" => 5,
+        "name" => "email",
+        "language" => "",
+        "type" => "text",
+        "data" => "info@pimcore.com",
+        "siteId" => 0,
+        "creationDate" => 0,
+        "modificationDate" => 1524042225
+    ]
+];

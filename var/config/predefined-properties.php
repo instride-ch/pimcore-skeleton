ae6a4dc8f5f53be1e5f8a84211c113b3181d3f72
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-11-12 13:32:21 +0100

    add more predefinied properties

--- var/config/predefined-properties.php
+++ var/config/predefined-properties.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 return [
     1 => [
@@ -13,5 +13,44 @@ return [
         "inheritable" => 1,
         "creationDate" => 0,
         "modificationDate" => 0
+    ],
+    2 => [
+        "id" => 2,
+        "name" => "User Email Document",
+        "description" => "Defines the email document, which is sent to the user.",
+        "key" => "userEmailDocument",
+        "type" => "document",
+        "data" => NULL,
+        "config" => NULL,
+        "ctype" => "document",
+        "inheritable" => FALSE,
+        "creationDate" => 1542022981,
+        "modificationDate" => 1542022981
+    ],
+    3 => [
+        "id" => 3,
+        "name" => "Admin Email Document",
+        "description" => "Defines the email document, which is sent to the admin.",
+        "key" => "adminEmailDocument",
+        "type" => "document",
+        "data" => NULL,
+        "config" => NULL,
+        "ctype" => "document",
+        "inheritable" => FALSE,
+        "creationDate" => 1542022981,
+        "modificationDate" => 1542022981
+    ],
+    4 => [
+        "id" => 4,
+        "name" => "Imprint Page",
+        "description" => "Defines where the Imprint page of the website lies.",
+        "key" => "imprintPage",
+        "type" => "document",
+        "data" => NULL,
+        "config" => NULL,
+        "ctype" => "document",
+        "inheritable" => TRUE,
+        "creationDate" => 1542022981,
+        "modificationDate" => 1542022981
     ]
 ];

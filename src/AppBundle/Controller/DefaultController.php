652818fd9bd3002ae00d2a02d6721d9abe60cb3b
Aaron Gerig <a.gerig@w-vision.ch>
2019-03-27 14:11:16 +0100

    Updated legals page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,12 +7,14 @@
  * For the full copyright and license information, please view the LICENSE.md
  * file that is distributed with this source code.
  *
- * @copyright  Copyright (c) 2018 w-vision AG (https://www.w-vision.ch)
+ * @copyright  Copyright (c) 2019 w-vision AG (https://www.w-vision.ch)
  */
 
 namespace AppBundle\Controller;
 
+use AppBundle\Helper\StringHelper;
 use Pimcore\Controller\FrontendController;
+use Pimcore\Model\Document;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends FrontendController
@@ -22,14 +24,38 @@ class DefaultController extends FrontendController
      */
     public function defaultAction(): Response
     {
+        $this->get('coreshop.seo.presentation')->updateSeoMetadata($this->document);
+
         return $this->renderTemplate('Default/default.html.twig');
     }
 
     /**
      * @return Response
      */
-    public function imprintAction(): Response
+    public function legalsAction(): Response
     {
-        return $this->renderTemplate('Default/imprint.html.twig');
+        $this->get('coreshop.seo.presentation')->updateSeoMetadata($this->document);
+
+        $scrollSpyNav = [];
+        $block = $this->document->getElement('legalsBlock');
+
+        if ($block instanceof Document\Tag\Block && !$block->isEmpty()) {
+            foreach ($block->getElements() as $item) {
+                $input = $item->getElement('itemTitle');
+
+                if (!$input instanceof Document\Tag\Input || $input->isEmpty()) {
+                    continue;
+                }
+
+                $scrollSpyNav[] = [
+                    'id' => StringHelper::toKebabCase($input->getName()),
+                    'data' => $input->getData(),
+                ];
+            }
+        }
+
+        return $this->renderTemplate('Default/legals.html.twig', [
+            'scrollSpyNav' => $scrollSpyNav,
+        ]);
     }
 }

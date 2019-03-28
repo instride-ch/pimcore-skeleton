68208eb5322c658294579ba0daff48bc1472428a
Aaron Gerig <a.gerig@w-vision.ch>
2019-03-28 11:18:39 +0100

    Rename search static route

--- var/config/staticroutes.php
+++ var/config/staticroutes.php
@@ -1,19 +1,22 @@
-<?php
+<?php 
 
 return [
     1 => [
         "id" => 1,
         "name" => "app_search",
-        "pattern" => "/^\\/([a-z]{2})\\/search/",
-        "reverse" => "/%_locale/search",
+        "pattern" => "/^\\/([a-z]{2})\\/suche/",
+        "reverse" => "/%_locale/suche",
         "module" => "AppBundle",
         "controller" => "@AppBundle\\Controller\\SearchController",
         "action" => "search",
         "variables" => "_locale",
-        "defaults" => null,
-        "siteId" => 0,
+        "defaults" => NULL,
+        "siteId" => [
+
+        ],
         "priority" => 1,
-        "creationDate" => 0,
-        "modificationDate" => 0
+        "legacy" => FALSE,
+        "creationDate" => 1553768287,
+        "modificationDate" => 1553768290
     ]
 ];

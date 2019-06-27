c2dc76c54f92663b97a111748237882da86eefd9
Aaron Gerig <a.gerig@w-vision.ch>
2019-06-27 18:55:57 +0200

    build: update deps and workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const { resolve } = require('path');
 const Encore = require('@symfony/webpack-encore');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const { version } = require('uikit/package.json');
 
@@ -14,7 +13,7 @@ const paths = {
 
 Encore
   // Set output and public paths
-  .setOutputPath(paths.output)
+  .setOutputPath(`${paths.output}/`)
   .setPublicPath(`/${paths.public}`)
   .setManifestKeyPrefix(`${paths.public}/`)
 
@@ -26,49 +25,58 @@ Encore
   .enableSingleRuntimeChunk()
   .enableEslintLoader()
   .splitEntryChunks()
-  .configureBabel(() => {}, { include_node_modules: ['uikit'] })
+  .configureBabel(() => {}, {
+    includeNodeModules: ['uikit'],
+    useBuiltIns: 'usage',
+    corejs: 3,
+  })
   .configureDefinePlugin((options) => {
     options.VERSION = JSON.stringify(version);
   })
   .addAliases({ 'uikit-util': `${paths.vendor}/uikit/src/js/util` })
-  .addRule({
-    test: /\.svg$/,
-    use: 'raw-loader'
-  })
 
   // CSS
   .addStyleEntry('css/global', `${paths.source}/scss/global.scss`)
   .addStyleEntry('css/email', `${paths.source}/scss/email.scss`)
   .addStyleEntry('css/editmode', `${paths.source}/scss/editmode.scss`)
-  .enableSassLoader((options) => {
-    options.includePaths = [
-      `${paths.vendor}/uikit/src/scss`,
-      `${paths.vendor}/foundation-emails/scss`,
-    ]
-  }, { resolveUrlLoader: false })
+  .enableSassLoader(() => {}, { resolveUrlLoader: false })
   .enablePostCssLoader()
   .addPlugin(new StyleLintPlugin())
 
   // Copy and optimize images
-  .disableImagesLoader()
   .copyFiles({
     from: `${paths.source}/images`,
     pattern: paths.pattern,
     to: 'images/[path][name].[hash:8].[ext]',
   })
-  .addPlugin(new ImageminPlugin({
-    disable: !Encore.isProduction(),
-    test: paths.pattern,
-  }))
 
   // Source maps and cache buster
   .enableSourceMaps(!Encore.isProduction())
   .enableVersioning(Encore.isProduction())
+
+  // Advanced config options
+  .configureWatchOptions((options) => {
+    options.poll = true;
+    options.ignored = `${paths.vendor}/`;
+  })
+  .configureLoaderRule('images', (rule) => {
+    rule.exclude = [
+      `${paths.vendor}/uikit`,
+      `${paths.source}/custom/icons`,
+    ];
+  })
+  .addRule({
+    test: /\.svg$/,
+    include: [
+      `${paths.vendor}/uikit`,
+      `${paths.source}/custom/icons`,
+    ],
+    loader: 'raw-loader'
+  })
 ;
 
 // Advanced webpack config
 const config = Encore.getWebpackConfig();
 config.optimization.concatenateModules = true;
-config.watchOptions = { poll: true, ignored: `${paths.vendor}/` };
 
 module.exports = config;

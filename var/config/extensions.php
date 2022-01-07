d04b470de86f2a65e73fcf3f55b1027cfe71870d
Aaron Gerig <a.gerig@w-vision.ch>
2022-01-07 16:09:17 +0100

    feat: finished SeoBundle and ToolboxBundle integrations

--- var/config/extensions.php
+++ var/config/extensions.php
@@ -4,7 +4,6 @@ return [
     "bundle" => [
         "SeoBundle\\SeoBundle" => TRUE,
         "EmailizrBundle\\EmailizrBundle" => TRUE,
-        "I18nBundle\\I18nBundle" => TRUE,
         "ToolboxBundle\\ToolboxBundle" => TRUE
     ]
 ];

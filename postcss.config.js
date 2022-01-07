ba868b52f5d85a221e12b0d53038d733d438d1a4
Aaron Gerig <a.gerig@w-vision.ch>
2022-01-07 15:20:43 +0100

    feat: added purgecss integration

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,17 @@
 const autoprefixer = require('autoprefixer');
-// const purgecss = require('@fullhuman/postcss-purgecss');
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
   plugins: [
     autoprefixer,
-    // purgecss({
-    //   content: ['./templates/**/*.html.twig'],
-    //   // Add other selectors to "safelist", which need to be included in the final CSS file.
-    //   // More info see: https://purgecss.com/safelisting.html
-    //   // safelist: ['example-selector'],
-    // }),
+    purgecss({
+      content: ['./templates/**/*.html.twig'],
+      // Add other selectors to "safelist", which need to be included in the final CSS file.
+      // More info see: https://purgecss.com/safelisting.html
+      safelist: {
+        standard: [/^uk-sticky/, 'uk-open', 'uk-active'],
+        deep: [/^(.+)([\\@]+)(.+)$/, /^uk-offcanvas/, /^uk-icon/],
+      },
+    }),
   ],
 };

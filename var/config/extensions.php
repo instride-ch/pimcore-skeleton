a829936cd616282f51aafdab3dcd10ae445f920d
aarongerig <a.gerig@w-vision.ch>
2022-04-13 16:53:59 +0200

    build: update composer dependencies and disable toolbox bricks by default

--- var/config/extensions.php
+++ var/config/extensions.php
@@ -6,5 +6,26 @@ return [
         "SeoBundle\\SeoBundle" => TRUE,
         "ToolboxBundle\\ToolboxBundle" => TRUE,
         "Wvision\\Bundle\\PimcoreMonitorBundle\\PimcoreMonitorBundle" => TRUE
+    ],
+    "areabrick" => [
+        "accordion" => FALSE,
+        "anchor" => FALSE,
+        "columns" => FALSE,
+        "container" => FALSE,
+        "content" => FALSE,
+        "download" => FALSE,
+        "gallery" => FALSE,
+        "googleMap" => FALSE,
+        "headline" => FALSE,
+        "iFrame" => FALSE,
+        "image" => FALSE,
+        "linkList" => FALSE,
+        "parallaxContainer" => FALSE,
+        "separator" => FALSE,
+        "slideColumns" => FALSE,
+        "snippet" => FALSE,
+        "spacer" => FALSE,
+        "teaser" => FALSE,
+        "video" => FALSE
     ]
 ];

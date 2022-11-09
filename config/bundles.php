ddf62bcc350a0e5e3ab77fcab5d0b257678c4a8e
aarongerig <a.gerig@w-vision.ch>
2022-11-09 11:05:06 +0100

    build: update dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
+    EmailizrBundle\EmailizrBundle::class => ['all' => true],
+    Wvision\Bundle\PimcoreMonitorBundle\PimcoreMonitorBundle::class => ['all' => true],
+    SeoBundle\SeoBundle::class => ['all' => true],
+    ToolboxBundle\ToolboxBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
